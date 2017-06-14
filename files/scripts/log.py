7a377387ebd923bb9995449a6161bad7c434f2fe
Markus Gabriel <e1326657@student.tuwien.ac.at>
2017-06-14 17:55:32 +0200

    Adjust logging to when there is no IP adress available.

--- files/scripts/log.py
+++ files/scripts/log.py
@@ -18,7 +18,14 @@ class Logger:
 
         if flow.client_conn.tls_established:
             with open(self.logfile, "a") as logfile:
-                logfile.write("host: " + flow.request.pretty_host + ",ip: " + flow.server_conn.ip_address.host + "\n")
+                logfile.write("server_conn address: " + flow.server_conn.address.host + "\n")
+                if flow.server_conn.ip_address:
+                    logfile.write("host: " + flow.request.pretty_host + ",ip: " + flow.server_conn.ip_address.host + "\n")
+                elif flow.server_conn.address:
+                    logfile.write("host: " + flow.request.pretty_host + ",ip: " + flow.server_conn.address.host + "\n")
+                else:
+                    logfile.write("host: " + flow.request.pretty_host + ",NO IP ADDRESS" + "\n")
+
                 logfile.write("url: " + flow.request.pretty_url + "\n")
         elif flow.server_conn.protocol == "http":
             with open(self.logfile, "a") as logfile:
